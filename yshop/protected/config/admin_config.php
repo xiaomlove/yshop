6502bb0426e00680db62afc4127c8d23eee36842
xiaomlove <353856593@qq.com>
2014-08-31 02:45:26 +0800

    修改过后必须全部提交？

--- yshop/protected/config/admin_config.php
+++ yshop/protected/config/admin_config.php
@@ -1,6 +1,38 @@
 <?php
 return array(
-	'navMenu'=>array(
-		'menu'=>array('title'=>'商品管理'),
-	)
+	'leftMenu'=>array(
+		array('title'=>'商品管理', 'model'=>'', 'action'=>'', 'subMenu'=>array(
+			array('title'=>'商品分类', 'model'=>'category', 'action'=>'list'),
+			array('title'=>'商品列表', 'model'=>'item', 'action'=>'list'),
+			array('title'=>'品牌列表', 'model'=>'brand', 'action'=>'index'),
+			array('title'=>'商品列表', 'model'=>'item', 'action'=>'list'),
+			array('title'=>'商品列表', 'model'=>'item', 'action'=>'list'),
+			array('title'=>'商品列表', 'model'=>'item', 'action'=>'list'),
+			array('title'=>'商品列表', 'model'=>'item', 'action'=>'list'),
+		)),
+		array('title'=>'用户管理', 'model'=>'', 'action'=>'', 'subMenu'=>array(
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+		)),
+		array('title'=>'订单管理', 'model'=>'', 'action'=>'', 'subMenu'=>array(
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+		)),
+		array('title'=>'系统设置', 'model'=>'', 'action'=>'', 'subMenu'=>array(
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+			array('title'=>'用户列表', 'model'=>'user', 'action'=>'list'),
+		)),
+	),
 );
